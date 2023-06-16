c1942fdff7b975d91570d6f750b0598d3aaaf06c
Memori-al <lki_familiar@naver.com>
2023-06-16 17:49:25 +0900

    sh 구조 및 디렉토리 변수 설정

--- sm-shell/Resource/Scripts/apm.sh
+++ sm-shell/Resource/Scripts/apm.sh
@@ -2,53 +2,58 @@
 
 _Split() {
     # Split Mysql.tar.gz
-    split -n 2 -d -a 1 /sm-shell/Resource/Data/mysql-5.6.24.tar.gz /sm-shell/Resource/Data/mysql-5.6.24.tar.gz.part-
-    rm -rf /sm-shell/Resource/Data/mysql-5.6.24.tar.gz
-    ls -al /sm-shell/Resource/Data/ | grep part*
+    split -n 2 -d -a 1 $data_path/mysql-5.6.24.tar.gz $data_path/mysql-5.6.24.tar.gz.part-
+    rm -rf $data_path/mysql-5.6.24.tar.gz
+    ls -al $data_path/ | grep part*
 }
 
 _Merge() {
     # merge Mysql.tar.gz
-    cat /sm-shell/Resource/Data/*part* > /sm-shell/Resource/Data/mysql-5.6.24.tar.gz
-    rm -rf /sm-shell/Resource/Data/*part*
-    ls -al /sm-shell/Resource/Data/ | grep mysql*
+    cat $data_path/*part* > $data_path/mysql-5.6.24.tar.gz
+    rm -rf $data_path/*part*
+    ls -al $data_path/ | grep mysql
 }
 
 _Ins() {
     # Install APM
-    packages=("httpd" "mariadb-server" "php'*'" "mariadb")
+    packages=("httpd" "mariadb-server" "php" "mariadb")
     for package in "${packages[@]}";
     do
         if [[ ! $(rpm -qa | grep $package) ]]; then
-            yum install -y $package --skip-broken
+            if [[ $package == "php" ]]; then
+                yum install -y $package* --skip-broken
+            else    
+                yum install -y $package
+            fi
         fi
     done
 
-    # Update Xe Src
-    if [[ -f "/var/www/html/xe.tar.gz" ]]; then
-        if [[ ! $(diff -q /sm-shell/Resource/Data/xe.tar.gz /var/www/html/xe.tar.gz) ]]; then
-            cp -rf /sm-shell/Resource/Data/xe.tar.gz /var/www/html
-            tar -xvf /var/www/html/xe.tar.gz -C /var/www/html
-            chmod -R 777 /var/www/html/xe
+    # Update $DIRECTORY Src
+    if [[ -f "/var/www/html/$FILES" ]]; then
+        if [[ ! $(diff -q $data_path/$FILES /var/www/html/$FILES) ]]; then
+            cp -rf $data_path/$FILES /var/www/html
+            tar -xvf /var/www/html/$FILES -C /var/www/html
+            chmod -R 777 /var/www/html/$DIRECTORY
         fi
     else
-        cp -rf /sm-shell/Resource/Data/xe.tar.gz /var/www/html
-        tar -xvf /var/www/html/xe.tar.gz -C /var/www/html
-        chmod -R 777 /var/www/html/xe
+        cp -rf $data_path/$FILES /var/www/html
+        tar -xvf /var/www/html/$FILES -C /var/www/html
+        chmod -R 777 /var/www/html/$DIRECTORY
     fi
 
     # Httpd Settings
-    if [[ ! $(diff -q /sm-shell/Resource/Data/httpd.conf /etc/httpd/conf/httpd.conf) ]]; then
-        cp -rf /sm-shell/Resource/Data/httpd.conf /etc/httpd/conf
+    if [[ ! $(diff -q $data_path/httpd.conf /etc/httpd/conf/httpd.conf) ]]; then
+        cp -rf $data_path/httpd.conf /etc/httpd/conf
         ls -al /etc/httpd/conf | grep httpd.conf
     fi
     
     # DB Restore
-    if [[ $(systemctl is-active mariadb) == "active" ]]; then
+    if [[ $(rpm -qa | grep mariadb) ]]; then
+        systemctl restart mariadb
         mysql_secure_installation
-        mysql -u root -p dkssud1@ < /sm-shell/Resource/Data/backup.dmp
+        mysql -u root -p < $data_path/backup.dmp
+        systemctl restart httpd
     fi
-    
 }
 
 # 오류 처리 함수
