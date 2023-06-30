7285ed391b7edc6b811587b8dd50e58985965943
Memori-al <lki_familiar@naver.com>
2023-06-30 21:10:11 +0900

    REFACTOR: 불 필요한 함수 제거 _Send_Global 함수 제거함

--- sm-shell/Resource/Scripts/mail.sh
+++ sm-shell/Resource/Scripts/mail.sh
@@ -1,34 +1,6 @@
 #!/bin/bash
 
 # DB Sorting 후 Score-result.txt 첨부 메일 송부
-_Send() {
-    domain="$NICK@$NICK.sm.jj.ac.kr"
-    subject="$ID-$NAME-리눅스-6번-문제"
-    body=$(cat <<-EOT
-        $NAME 메일 송부 드립니다.
-        한 학기 동안 감사했습니다!
-        (별첨) score-result.txt
-EOT
-)
-    attachment="/sm-shell/score_result.txt"
-
-    echo "From: $domain"
-    echo "To: $TO, $domain"
-    echo "Subject: $subject"
-    echo "Content-Type: multipart/mixed; boundary=XYZ"
-    echo ""
-    echo "--XYZ"
-    echo "Content-Type: text/plain; charset=utf-8"
-    echo ""
-    echo "$body"
-    echo "--XYZ"
-    echo "Content-Type: text/plain"
-    echo "Content-Disposition: attachment; filename=\"score-result.txt\""
-    echo ""
-    cat "$attachment"
-    echo "--XYZ--"
-}
-
 _Send_root() {
     subject="$ID-$NAME-리눅스-6번-문제"
     body=$(cat <<-EOT
@@ -88,16 +60,7 @@ _Minstall() {
 
 # 전달된 파라미터 필터링
 if [[ "$1" == "send" ]]; then
-    if [[ "$2" == "global" ]]; then
-        if [[ $(systemctl is-active sendmail) ]]; then
-            _Send | /usr/sbin/sendmail -t
-            echo 
-            echo -e "$white$b_green"" Complete $cls  Mail transfer to "$line"$TO$cls"
-            echo 
-        else
-            _Handler sendmail "$(date '+%H:%M:%S')" service
-        fi
-    elif [[ "$2" == "local" ]]; then
+    if [[ "$2" == "local" ]]; then
         if [[ $(systemctl is-active sendmail) ]]; then
             _Send_root | /usr/sbin/sendmail -t
             echo 
